ab49f1d99b56fb0cf81cfafab39be36740b403ff
Aliss55 <alicelh1600@outlook.com>
2024-02-04 01:08:28 -0600

    - Added theme switcher service refactoring - Added css files for light and dark themes

---
+++ FrontEnd/TerminalProject/src/app/services/theme-switcher.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Subject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeSwitcherService {
+
+  private theme$ = new BehaviorSubject<string>('light');
+
+  public initTheme(): void {
+    const storedTheme:string | null = localStorage.getItem('theme$');
+    if (storedTheme) {
+      this.theme$.next(storedTheme);
+    }
+    this.createLinkToStylesheet();
+    this.updateLinkToStylesheet();
+  }
+  toggleTheme(): void {
+    this.theme$.next(this.theme$.value === 'light' ? 'dark' : 'light');
+    localStorage.setItem('theme$', this.theme$.value);
+    this.updateLinkToStylesheet();
+  }
+
+  private createLinkToStylesheet(): void {
+    const link = document.createElement('link');
+    link.id = 'theme$';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+  }
+
+    private updateLinkToStylesheet(): void {
+    const link = document.getElementById('theme$') as HTMLLinkElement;
+    if (link) {
+      link.href = `/assets/themes/${this.theme$.value}-theme.css`;
+    }
+  }
+
+}
+
+
+
+
+
+
