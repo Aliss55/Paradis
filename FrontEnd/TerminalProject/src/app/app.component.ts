b209472528d14aa27e88ae7a5765c21bef76c332
Aliss55 <alicelh1600@outlook.com>
2024-02-04 00:51:25 -0600

    Added theme switcher

--- FrontEnd/TerminalProject/src/app/app.component.ts
+++ FrontEnd/TerminalProject/src/app/app.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
+import {ThemeSwitcherService} from "./services/theme-switcher.service";
+import {BehaviorSubject, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TerminalProject';
 
-  constructor(translateService: TranslateService){
+  constructor(translateService: TranslateService, private themeService: ThemeSwitcherService){
     translateService.setDefaultLang('es');
     translateService.use('es');
   }
+
+  ngOnInit(): void {
+      this.themeService.initTheme();
+  }
+
+    toggleTheme(): void{
+    this.themeService.toggleTheme();
+    }
+
 }
