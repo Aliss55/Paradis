3de06f675e44761273ee764713aa34b828cc683a
Aliss55 <alicelh1600@outlook.com>
2024-02-04 01:10:58 -0600

    - Deleted unused import

--- FrontEnd/TerminalProject/src/app/app.component.ts
+++ FrontEnd/TerminalProject/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 import {ThemeSwitcherService} from "./services/theme-switcher.service";
-import {BehaviorSubject, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-root',
