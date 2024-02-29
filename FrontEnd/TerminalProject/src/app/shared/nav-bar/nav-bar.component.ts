0680ba540c62a19a4b32c558b092ff20ac05ce81
Mdperez19 <mdperezpriego@gmail.com>
2024-02-28 23:22:17 -0600

    - Alt added - Import unused removed

--- FrontEnd/TerminalProject/src/app/shared/nav-bar/nav-bar.component.ts
+++ FrontEnd/TerminalProject/src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ThemeSwitcherService } from '../../services/theme-switcher.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-nav-bar',
