b22f4a3426aaf06b754a75666e6327c11a62d039
Aliss55 <alicelh1600@outlook.com>
2024-03-07 16:32:19 -0600

    Changes by mdper

--- FrontEnd/TerminalProject/src/app/components/Blog/blog.module.ts
+++ FrontEnd/TerminalProject/src/app/components/Blog/blog.module.ts
@@ -9,7 +9,9 @@ import {BlogRoutingModule} from "./blog-routing.module";
 import {HttpClient} from "@angular/common/http";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
-
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http);
+// }
 @NgModule({
   declarations: [BlogComponent],
   imports: [
