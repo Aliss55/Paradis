21d6990f066b73ecb88409268d7be8c5b146a758
Aliss55 <alicelh1600@outlook.com>
2024-03-07 16:29:07 -0600

    - imported modules

--- FrontEnd/TerminalProject/src/app/components/Blog/blog.module.ts
+++ FrontEnd/TerminalProject/src/app/components/Blog/blog.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BlogComponent } from './Pages/blog/blog.component';
+import {TranslateModule} from '@ngx-translate/core';
+import { PrimeNGModule } from '../../styling/prime-ng.module';
+import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import {BlogRoutingModule} from "./blog-routing.module";
+import {HttpClient} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BlogComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+   TranslateModule,
+    PrimeNGModule,
+    SharedModule,
+    RouterModule,
+    RouterModule,
+    BlogRoutingModule
+  ],
+  exports:[
+  ],
+  providers: [],
 })
-export class BlogModule { }
+export class BlogModule {}
