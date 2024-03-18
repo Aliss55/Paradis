4fcc6d946d111acde5adc87f83935dec8fc18ebb
Mdperez19 <mdperezpriego@gmail.com>
2024-03-18 16:46:32 -0600

    Update app-menu-options in blog.component.html and add options in blog.component.ts

--- FrontEnd/TerminalProject/src/app/components/Blog/Pages/blog/blog.component.ts
+++ FrontEnd/TerminalProject/src/app/components/Blog/Pages/blog/blog.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import {Options} from "../../../../interfaces/options";
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
 })
 export class BlogComponent {
+  public blogs :Options[]= [
+    {
+      name: 'Oraciones',
+      description: '¿Que son las oraciones?',
+      image: './assets/home-images/pingu.jpg',
+      link: '/home',
+    },
+    {
+      name: 'Blog 1',
+      description: 'in this blo we will talk about the latest news',
+      image: './assets/home-images/Blog-Igu.svg',
+      link: '/home',
+    },
+    {
+      name: 'Blog 2',
+      description: 'in this blo we will talk about the latest news',
+      image: './assets/home-images/igu-actividades.svg',
+      link: '/home',
+    },
+  ];
 
 }
