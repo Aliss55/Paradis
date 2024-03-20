9310e989d7f3bff3a01aaddd88bd59582931ca2c
Aliss55 <alicelh1600@outlook.com>
2024-03-19 20:05:30 -0600

    - Added complete sentences activity files

---
+++ FrontEnd/TerminalProject/src/app/components/activities/Pages/complete-sentences-activity/complete-sentences-activity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complete-sentences-activity',
+  templateUrl: './complete-sentences-activity.component.html',
+  styleUrl: './complete-sentences-activity.component.scss'
+})
+export class CompleteSentencesActivityComponent {
+
+}
