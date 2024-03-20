239b6401552747b9cf1f864891addd43b53d58ce
Aliss55 <alicelh1600@outlook.com>
2024-03-19 20:56:46 -0600

    added route for activities

--- FrontEnd/TerminalProject/src/app/components/activities/activities-routing.module.ts
+++ FrontEnd/TerminalProject/src/app/components/activities/activities-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ActivitiesComponent} from "./Pages/activities/activities.component";
+import { ActivitiesComponent } from './Pages/activities/activities.component';
+import { CompleteSentencesActivityComponent } from './Pages/complete-sentences-activity/complete-sentences-activity.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ActivitiesComponent
-  }
+    component: ActivitiesComponent,
+  },
+  {
+    path: 'complete-sentences',
+    component: CompleteSentencesActivityComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ActivitiesRoutingModule { }
+export class ActivitiesRoutingModule {}
