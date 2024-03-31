18cc01c9fb747174a97e568f1057d53553e6c8d7
Aliss55 <alicelh1600@outlook.com>
2024-03-30 22:18:13 -0600

    updated shared module to declare and export BlogEntryTemplate

--- FrontEnd/TerminalProject/src/app/shared/shared.module.ts
+++ FrontEnd/TerminalProject/src/app/shared/shared.module.ts
@@ -1,30 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {NavBarComponent} from "./nav-bar/nav-bar.component";
-import {PrimeNGModule} from "../styling/prime-ng.module";
-import { TranslateModule} from "@ngx-translate/core";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PrimeNGModule } from '../styling/prime-ng.module';
+import { TranslateModule } from '@ngx-translate/core';
 import { MenuOptionsComponent } from './menu-options/menu-options.component';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule} from "@angular/common/http";
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { BlogEntryTemplateComponent } from './blog-entry-template/blog-entry-template.component';
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    MenuOptionsComponent
+    MenuOptionsComponent,
+    BlogEntryTemplateComponent,
   ],
-    imports: [
-        CommonModule,
-        PrimeNGModule,
-        RouterModule,
-      HttpClientModule,
-      TranslateModule
-
-    ],
-  exports:[
-    NavBarComponent,
-    MenuOptionsComponent
-  ]
+  imports: [
+    CommonModule,
+    PrimeNGModule,
+    RouterModule,
+    HttpClientModule,
+    TranslateModule,
+  ],
+  exports: [NavBarComponent, MenuOptionsComponent, BlogEntryTemplateComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
