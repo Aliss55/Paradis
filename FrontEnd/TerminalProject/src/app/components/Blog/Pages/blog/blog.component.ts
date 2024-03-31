77338ae70588006c6bc6eb8039fd2ca0c082eeff
Aliss55 <alicelh1600@outlook.com>
2024-03-30 22:16:00 -0600

    updated routing data

--- FrontEnd/TerminalProject/src/app/components/Blog/Pages/blog/blog.component.ts
+++ FrontEnd/TerminalProject/src/app/components/Blog/Pages/blog/blog.component.ts
@@ -1,30 +1,29 @@
 import { Component } from '@angular/core';
-import {Options} from "../../../../interfaces/options";
+import { Options } from '../../../../interfaces/options';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.scss'
+  styleUrl: './blog.component.scss',
 })
 export class BlogComponent {
-  public blogs :Options[]= [
+  public blogs: Options[] = [
     {
       name: 'Oraciones',
       description: '¿Que son las oraciones?',
       image: './assets/home-images/pingu.jpg',
-      link: '/home',
+      link: '/blog/sentences-1',
     },
     {
       name: 'Blog 1',
       description: 'in this blo we will talk about the latest news',
       image: './assets/home-images/Blog-Igu.svg',
-      link: '/home',
+      link: '/blog_1',
     },
     {
       name: 'Blog 2',
       description: 'in this blo we will talk about the latest news',
       image: './assets/home-images/igu-actividades.svg',
-      link: '/home',
+      link: '/blog_1',
     },
   ];
-
 }
