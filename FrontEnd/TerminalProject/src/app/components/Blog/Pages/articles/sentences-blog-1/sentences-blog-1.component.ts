0657ba4e5d0a0b12cabe23e2a2d26df4c96645b9
Aliss55 <alicelh1600@outlook.com>
2024-03-30 22:13:02 -0600

    added new sentences component

---
+++ FrontEnd/TerminalProject/src/app/components/Blog/Pages/articles/sentences-blog-1/sentences-blog-1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sentences-blog-1',
+  templateUrl: './sentences-blog-1.component.html',
+  styleUrl: './sentences-blog-1.component.scss'
+})
+export class SentencesBlog1Component {
+
+}
