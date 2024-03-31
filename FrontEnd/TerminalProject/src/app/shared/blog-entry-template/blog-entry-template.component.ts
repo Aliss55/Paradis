153a4c22bfb10dc59aad6249f893827306c6125d
Aliss55 <alicelh1600@outlook.com>
2024-03-30 22:17:49 -0600

    added blog entry template

---
+++ FrontEnd/TerminalProject/src/app/shared/blog-entry-template/blog-entry-template.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-entry-template',
+  templateUrl: './blog-entry-template.component.html',
+  styleUrl: './blog-entry-template.component.scss'
+})
+export class BlogEntryTemplateComponent {
+
+}
