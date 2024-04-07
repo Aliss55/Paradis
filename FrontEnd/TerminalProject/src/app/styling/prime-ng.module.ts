41ff793aa0ae4cce52ce121870b19b645e561279
Aliss55 <alicelh1600@outlook.com>
2024-04-06 23:41:35 -0600

    drag and drop with unique index works

--- FrontEnd/TerminalProject/src/app/styling/prime-ng.module.ts
+++ FrontEnd/TerminalProject/src/app/styling/prime-ng.module.ts
@@ -5,25 +5,26 @@ import { DockModule } from 'primeng/dock';
 import { NgModule } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [],
   imports: [
-
     ButtonModule,
     CardModule,
     CommonModule,
     DockModule,
     SidebarModule,
     ToolbarModule,
+    TableModule,
   ],
   exports: [
-
     ButtonModule,
     CardModule,
     DockModule,
     SidebarModule,
     ToolbarModule,
+    TableModule,
   ],
 })
 export class PrimeNGModule {}
