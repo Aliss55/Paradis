135d6a2d79309a26d76275757c7dff796b53f613
Aliss55 <alicelh1600@outlook.com>
2024-04-21 21:53:32 -0600

    added template for about us development team layout

--- FrontEnd/TerminalProject/src/app/components/AboutUs/about-us.module.ts
+++ FrontEnd/TerminalProject/src/app/components/AboutUs/about-us.module.ts
@@ -4,6 +4,7 @@ import {PrimeNGModule} from "../../styling/prime-ng.module";
 import {AboutUsComponent} from "./Pages/about-us/about-us.component";
 import {TranslateModule} from "@ngx-translate/core";
 import {AboutUsRoutingModule} from "./about-us-routing.module";
+import {StyleClassModule} from "primeng/styleclass";
 
 
 
@@ -11,11 +12,12 @@ import {AboutUsRoutingModule} from "./about-us-routing.module";
   declarations: [
     AboutUsComponent
   ],
-  imports: [
-    CommonModule,
-    AboutUsRoutingModule,
-    PrimeNGModule,
-    TranslateModule
-  ]
+    imports: [
+        CommonModule,
+        AboutUsRoutingModule,
+        PrimeNGModule,
+        TranslateModule,
+        StyleClassModule
+    ]
 })
 export class AboutUsModule { }
