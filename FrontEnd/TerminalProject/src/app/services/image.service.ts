00a0bd9f94e8c0219db53a77fe19309de3b15bb0
Aliss55 <alicelh1600@outlook.com>
2024-04-27 22:34:21 -0600

    added image service for updating images whenever the theme changes

---
+++ FrontEnd/TerminalProject/src/app/services/image.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {Theme, ThemeSwitcherService} from "./theme-switcher.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ImageService {
+
+    constructor(private themeService: ThemeSwitcherService) {}
+
+    public getImagePathAccordingToTheme(image_name: string, image_path: string): string {
+        console.log(this.themeService.currentAppTheme)
+        if (this.themeService.currentAppTheme === Theme.LIGHT) {
+            return image_path + '/' + image_name
+        } else {
+            return image_path + '/' + "Dark-Mode-" + image_name
+        }
+    }
+}
