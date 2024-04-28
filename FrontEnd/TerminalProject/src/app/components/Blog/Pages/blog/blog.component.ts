93e8f903e566fdbb3e65ffdeb4cec905d1c3f2d0
Aliss55 <alicelh1600@outlook.com>
2024-04-27 21:58:31 -0600

    Updated image selector in blogs component

--- FrontEnd/TerminalProject/src/app/components/Blog/Pages/blog/blog.component.ts
+++ FrontEnd/TerminalProject/src/app/components/Blog/Pages/blog/blog.component.ts
@@ -1,48 +1,72 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import { Options } from '../../../../interfaces/options';
+import {ThemeSwitcherService} from "../../../../services/theme-switcher.service";
+import {Subscription} from "rxjs";
+import {ImageService} from "../../../../services/image.service";
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss',
 })
-export class BlogComponent {
-  public blogs: Options[] = [
+export class BlogComponent implements OnDestroy{
+
+  public blogs: Options[] = [];
+  private themeSubscription: Subscription;
+
+
+  constructor(private imageService: ImageService, private themeService: ThemeSwitcherService) {
+    this.themeSubscription = this.themeService.actualTheme.subscribe(()=>
+    this.blogs= this.getBlogs())
+  }
+
+
+private getBlogs(): Options[]{
+
+
+  return [
     {
       name: 'BLOG_LABELS.BLOG_1.TITLE',
       description: 'BLOG_LABELS.BLOG_1.DESCRIPTION',
-      image: './assets/blogs-images/Oracion - 220x220.svg',
+      image: this.imageService.getImagePathAccordingToTheme('Blog-1-50px.svg', './assets/blogs-images'),
       link: '/blog/sentences-1',
     },
     {
       name: 'BLOG_LABELS.BLOG_2.TITLE',
       description: 'BLOG_LABELS.BLOG_2.DESCRIPTION',
-      image: './assets/home-images/pingu.jpg',
+      image: this.imageService.getImagePathAccordingToTheme('Blog-2-50px.svg', './assets/blogs-images'),
       link: '/blog/period',
     },
     {
       name: 'BLOG_LABELS.BLOG_3.TITLE',
       description: 'BLOG_LABELS.BLOG_3.DESCRIPTION',
-      image: './assets/home-images/pingu.jpg',
+      image: this.imageService.getImagePathAccordingToTheme('Blog-3-50px.svg', './assets/blogs-images'),
       link: '/blog/comma',
     },
     {
       name: 'BLOG_LABELS.BLOG_4.TITLE',
       description: 'BLOG_LABELS.BLOG_4.DESCRIPTION',
-      image: './assets/home-images/pingu.jpg',
+      image: this.imageService.getImagePathAccordingToTheme('Blog-1-50px.svg', './assets/blogs-images'),
       link: '/blog/capitalization',
     },
     {
       name: 'BLOG_LABELS.BLOG_5.TITLE',
       description: 'BLOG_LABELS.BLOG_5.DESCRIPTION',
-      image: './assets/home-images/pingu.jpg',
+      image: this.imageService.getImagePathAccordingToTheme('Blog-2-50px.svg', './assets/blogs-images'),
       link: '/blog/halla-haya-aya-alla-difference',
     },
     {
       name: 'BLOG_LABELS.BLOG_6.TITLE',
       description: 'BLOG_LABELS.BLOG_6.DESCRIPTION',
-      image: './assets/home-images/pingu.jpg',
+      image: this.imageService.getImagePathAccordingToTheme('Blog-3-50px.svg', './assets/blogs-images'),
       link: '/blog/adverbs',
     },
 
   ];
+}
+  ngOnDestroy(): void {
+        this.themeSubscription.unsubscribe();
+    }
+
+
 }
