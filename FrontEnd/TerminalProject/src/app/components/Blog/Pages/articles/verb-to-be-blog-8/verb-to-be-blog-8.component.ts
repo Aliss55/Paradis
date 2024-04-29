e2b396d3a1ace733eb37d367baeaace0caca0120
Aliss55 <alicelh1600@outlook.com>
2024-04-28 23:41:41 -0600

    - added animations for blogs 7,8,9 - added original scroll position in blogs 7,8,9

--- FrontEnd/TerminalProject/src/app/components/Blog/Pages/articles/verb-to-be-blog-8/verb-to-be-blog-8.component.ts
+++ FrontEnd/TerminalProject/src/app/components/Blog/Pages/articles/verb-to-be-blog-8/verb-to-be-blog-8.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ViewportScroller} from "@angular/common";
 
 @Component({
   selector: 'app-verb-to-be-blog-8',
   templateUrl: './verb-to-be-blog-8.component.html',
   styleUrl: './verb-to-be-blog-8.component.scss'
 })
-export class VerbToBeBlog8Component {
+export class VerbToBeBlog8Component implements OnInit{
+
+  constructor(private viewportScrollerService: ViewportScroller) {
+  }
+
+  ngOnInit(): void {
+        this.viewportScrollerService.scrollToPosition([0, 0]);
+    }
 
 }
