e9cd6d69b83a454b94596da2ea0765b84996f470
Aliss55 <alicelh1600@outlook.com>
2024-05-05 13:56:58 -0600

    added images and i18n to the activities menu

--- FrontEnd/TerminalProject/src/app/components/activities/Pages/activities/activities.component.ts
+++ FrontEnd/TerminalProject/src/app/components/activities/Pages/activities/activities.component.ts
@@ -1,37 +1,68 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Options } from '../../../../interfaces/options';
-import {ViewportScroller} from "@angular/common";
+import { ViewportScroller } from '@angular/common';
+import { ImageService } from '../../../../services/image.service';
+import { ThemeSwitcherService } from '../../../../services/theme-switcher.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss',
 })
-export class ActivitiesComponent implements OnInit{
-  public activities: Options[] = [
-    {
-      name: 'Oraciones incompletas',
-      description: 'Completa las oraciones',
-      image: './assets/home-images/pingu.jpg',
-      link: '/activities/complete-sentences',
-    },
-    {
-      name: 'Profesiones y ocupaciones',
-      description: 'in this blo we will talk about the latest news',
-      image: './assets/home-images/Blog-Igu.svg',
-      link: '/activities/professions-and-occupations',
-    },
-    {
-      name: 'Actividades 2',
-      description: 'in this blo we will talk about the latest news',
-      image: './assets/home-images/igu-actividades.svg',
-      link: '/home',
-    },
-  ];
+export class ActivitiesComponent implements OnInit, OnDestroy{
 
-  constructor(private viewportScrollerService: ViewportScroller) {}
+  public activities: Options[] = [];
+  private themeSubscription: Subscription;
+
+  constructor(
+    private imageService: ImageService,
+    private themeService: ThemeSwitcherService,
+    private viewportScrollerService: ViewportScroller,
+  ) {
+    this.themeSubscription = this.themeService.actualTheme.subscribe(
+      () => (this.activities = this.getActivites()),
+    );
+  }
 
   ngOnInit(): void {
     this.viewportScrollerService.scrollToPosition([0, 0]);
+  }
+
+
+  private getActivites(): Options[] {
+    return [
+      {
+        name: 'ACTIVITIES_LABELS.CARDS_GAME.PROFESSIONS_OCCUPATIONS',
+        description: 'ACTIVITIES_LABELS.CARDS_GAME.PROFESSIONS_OCCUPATIONS_CARD_DESCRIPTION',
+        image: this.imageService.getImagePathAccordingToTheme(
+          'Penguin-3-50px.svg',
+          './assets/penguin-images',
+        ),
+        link: '/activities/professions-and-occupations',
+      },
+      {
+        name: 'ACTIVITIES_LABELS.CARDS_GAME.DEMONYMS',
+        description: 'ACTIVITIES_LABELS.CARDS_GAME.DEMONYMS_CARD_DESCRIPTION',
+        image: this.imageService.getImagePathAccordingToTheme(
+          'Penguin-2-50px.svg',
+          './assets/penguin-images',
+        ),
+        link: '/activities/complete-sentences',
+      },
+      {
+        name: 'Actividades 2',
+        description: 'in this blo we will talk about the latest news',
+        image: this.imageService.getImagePathAccordingToTheme(
+          'Penguin-1-50px.svg',
+          './assets/penguin-images',
+        ),
+        link: '/home',
+      },
+    ];
+  };
+
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
   }
 }
