f0a2f4bb172ad023a68000e21d21be6f5d4ce11d
Mario Daniel Pérez Priego <69879158+Mdperez19@users.noreply.github.com>
2024-05-12 14:45:55 -0600

    Update wordSuggesterService URL

--- FrontEnd/TerminalProject/src/environments/environment.prod.ts
+++ FrontEnd/TerminalProject/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   spellCheckerService: 'https://sina-tt-api.azurewebsites.net',
-  wordSuggesterService: 'http://maria-tt-api.azurewebsites.net',
+  wordSuggesterService: 'https://maria-tt-api.azurewebsites.net',
   production: true,
 }
