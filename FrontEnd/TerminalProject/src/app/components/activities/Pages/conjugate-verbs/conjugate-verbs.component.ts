18cafcaf440315374904155aee52949f9e86072a
Aliss55 <alicelh1600@outlook.com>
2024-05-17 22:18:02 -0600

    added components

---
+++ FrontEnd/TerminalProject/src/app/components/activities/Pages/conjugate-verbs/conjugate-verbs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conjugate-verbs',
+  templateUrl: './conjugate-verbs.component.html',
+  styleUrl: './conjugate-verbs.component.scss'
+})
+export class ConjugateVerbsComponent {
+
+}
