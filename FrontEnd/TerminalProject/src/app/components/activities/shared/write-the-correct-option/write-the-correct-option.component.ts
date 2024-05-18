1f6aaccabae4b8a52801d1842fbc66bc2a9da8e2
Aliss55 <alicelh1600@outlook.com>
2024-05-17 20:25:23 -0600

    added modal

--- FrontEnd/TerminalProject/src/app/components/activities/shared/write-the-correct-option/write-the-correct-option.component.ts
+++ FrontEnd/TerminalProject/src/app/components/activities/shared/write-the-correct-option/write-the-correct-option.component.ts
@@ -1,25 +1,36 @@
-import {ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges} from '@angular/core';
-import {orthography_options} from "../../Pages/utils/Orthography_options";
-import {ShufflerService} from "../services/shuffler.service";
-import {ThemeSwitcherService} from "../../../../services/theme-switcher.service";
-import {NotificationService} from "../services/notification-service.service";
-import {MessageService} from "primeng/api";
+import {
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { orthography_options } from '../../Pages/utils/Orthography_options';
+import { ShufflerService } from '../services/shuffler.service';
+import { ThemeSwitcherService } from '../../../../services/theme-switcher.service';
+import { NotificationService } from '../services/notification-service.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-write-the-correct-option',
   templateUrl: './write-the-correct-option.component.html',
-  styleUrl: './write-the-correct-option.component.scss'
+  styleUrl: './write-the-correct-option.component.scss',
 })
-export class WriteTheCorrectOptionComponent implements OnChanges{
+export class WriteTheCorrectOptionComponent implements OnChanges {
   @Input()
-  public questions: orthography_options[] = []
+  public questions: orthography_options[] = [];
   public isCorrect: boolean[] = [];
-  public labels: string[][]=[];
-  public shuffledQuestions: string[][] = []
+  public labels: string[][] = [];
+  public shuffledQuestions: string[][] = [];
   public answer: string[] = [];
+  visible: boolean = true;
 
-  constructor( public shufflerService: ShufflerService, private themeService : ThemeSwitcherService, public notificationService : NotificationService, private cdr: ChangeDetectorRef) {
-      }
+  constructor(
+    public shufflerService: ShufflerService,
+    private themeService: ThemeSwitcherService,
+    public notificationService: NotificationService,
+    private cdr: ChangeDetectorRef,
+  ) {}
   ngOnChanges(changes: SimpleChanges) {
     if (changes['questions'] && changes['questions'].currentValue) {
       this.labels = this.shufflerService.getShuffledPropertyLabels(
@@ -35,18 +46,26 @@ export class WriteTheCorrectOptionComponent implements OnChanges{
       this.cdr.detectChanges();
     }
   }
-  public checkAnswer(index_card:number): void {
-    if(this.answer[index_card] === '' || this.answer[index_card] === undefined){
-      this.notificationService.notifyError('Por favor introduce una respuesta válida');
+  public checkAnswer(index_card: number): void {
+    if (
+      this.answer[index_card] === '' ||
+      this.answer[index_card] === undefined
+    ) {
+      this.notificationService.notifyError(
+        'Por favor introduce una respuesta válida',
+      );
       return;
     }
     if (this.answer[index_card] === this.questions[index_card].correct_word) {
       this.isCorrect[index_card] = true;
-      this.notificationService.notifySucess("¡Respuesta correcta!");
+      this.notificationService.notifySucess('¡Respuesta correcta!');
     } else {
       this.notificationService.notifyError('¡Inténtalo de nuevo!');
       this.answer[index_card] = '';
     }
   }
 
+  public showOrthographicGuide(): void {
+    this.visible = !this.visible;
+  }
 }
