998ecbce0ac780f00cfdd0617cc0efec726a2f61
Aliss55 <alicelh1600@outlook.com>
2024-05-17 22:17:43 -0600

    routed component

---
+++ FrontEnd/TerminalProject/src/app/components/activities/Pages/utils/Verb_conjugation_by_subject.ts
@@ -0,0 +1,11 @@
+export interface verb_conjugation_by_subject {
+    verb_infinitive_form: string;
+    singular_first_person_yo_form: string;
+    singular_second_person_tu_form: string;
+    singular_third_person_el_form: string;
+    singular_third_person_ella_form: string;
+    plural_first_person_nosotros_form: string;
+    plural_second_person_ustedes_form: string;
+    plural_third_person_ellos_form: string;
+    plural_third_person_ellas_form: string;
+}
