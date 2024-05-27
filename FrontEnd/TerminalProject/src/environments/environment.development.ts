1411e998aee0cacf301927ec0be34e8c9a14497c
Mdperez19 <mdperezpriego@gmail.com>
2024-05-26 21:00:39 -0600

    Delete environmets from the repo

--- FrontEnd/TerminalProject/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  spellCheckerService: 'http://127.0.0.1:5002',
-  wordSuggesterService: 'http://127.0.0.1:5000',
-  grammaticalAnalyzerService: 'http://127.0.0.1:5001',
-  production: false,
-}
