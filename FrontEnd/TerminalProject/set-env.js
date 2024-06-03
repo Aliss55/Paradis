a2705e1ff142afbda610e9c7f294de7d29224fab
Mario Daniel Pérez Priego <69879158+Mdperez19@users.noreply.github.com>
2024-06-02 19:42:00 -0600

    Update set-env.js

--- FrontEnd/TerminalProject/set-env.js
+++ FrontEnd/TerminalProject/set-env.js
@@ -13,7 +13,7 @@ const envFile = `export const environment = {
   production: true,
 };`
 
-const targetPath = path.join(__dirname, './src/environments/environment.prod.ts');
+const targetPath = path.join(process.cwd(), './src/environments/environment.prod.ts');
 fs.writeFile(targetPath, envFile, (err) => {
   if (err) {
     console.error(err);
