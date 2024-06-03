96171a3548d18ca98d34fb2e368d8d0d46f24e55
Mario Daniel Pérez Priego <69879158+Mdperez19@users.noreply.github.com>
2024-06-02 21:10:24 -0600

    Create environment.prod.ts

---
+++ FrontEnd/TerminalProject/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  spellCheckerService: '',
+  wordSuggesterService: '',
+  grammaticalAnalyzerService: '',
+  OPENAI_API_KEY: '',
+  moderationService: '',
+  production: false,
+};
