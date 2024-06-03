f9b4e9aee390c233c33b581f8f2f7e63f67fd515
Mario Daniel Pérez Priego <69879158+Mdperez19@users.noreply.github.com>
2024-06-02 20:09:15 -0600

    Update set-env.js

--- FrontEnd/TerminalProject/set-env.js
+++ FrontEnd/TerminalProject/set-env.js
@@ -1,46 +1,29 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
-const dotenv = require('dotenv').config({ path: 'src/.env' });
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({path: 'src/.env'}); ;
 
-(async () => {
-  try {
-    // Validar que todas las variables de entorno necesarias estén definidas
-    const requiredEnvVars = [
-      'spellCheckerService',
-      'wordSuggesterService',
-      'grammaticalAnalyzerService',
-      'OPENAI_API_KEY',
-      'moderationService'
-    ];
-    requiredEnvVars.forEach(varName => {
-      if (!process.env[varName]) {
-        throw new Error(`Variable de entorno requerida no definida: ${varName}`);
-      }
-    });
+const envFile = `export const environment = {
+  spellCheckerService: '${process.env.spellCheckerService}',
+  wordSuggesterService: '${process.env.wordSuggesterService}',
+  grammaticalAnalyzerService: '${process.env.grammaticalAnalyzerService}',
+  OPENAI_API_KEY: '${process.env.OPENAI_API_KEY}',
+  moderationService: '${process.env.moderationService}',
+  production: true,
+};`
 
-    // Generar contenido del archivo environment.prod.ts
-    const envFileContent = `export const environment = {
-      spellCheckerService: '${process.env.spellCheckerService}',
-      wordSuggesterService: '${process.env.wordSuggesterService}',
-      grammaticalAnalyzerService: '${process.env.grammaticalAnalyzerService}',
-      OPENAI_API_KEY: '${process.env.OPENAI_API_KEY}',
-      moderationService: '${process.env.moderationService}',
-      production: true,
-    };`;
+// crear la carpeta environments si no existe
+if (!fs.existsSync('./src/environments')) {
+  fs.mkdirSync('./src/environments');
+}
 
-    // Crear la carpeta environments si no existe
-    const envDirPath = path.join(__dirname, 'src', 'environments');
-    await fs.mkdir(envDirPath, { recursive: true });
-
-    // Escribir el archivo environment.prod.ts
-    const targetPath = path.join(envDirPath, 'environment.ts');
-    await fs.writeFile(targetPath, envFileContent);
-
-    console.log('\x1b[32m%s\x1b[0m', '\u{2705} Successfully generated environment.ts');
-
-    // Ejecutar la compilación de Angular
-    require('child_process').execSync('ng build --configuration production', { stdio: 'inherit' });
-  } catch (err) {
-    console.error('Error al generar el archivo de entorno:', err);
+const targetPath = path.join(__dirname, './src/environments/environment.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(successColor, `${checkSign} Successfully generated environment.ts`);
   }
-})();
+});
